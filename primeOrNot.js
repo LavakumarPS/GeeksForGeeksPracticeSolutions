29f73a22c315b755d0f7acfea9d755ea862cd855
LavakumarPS <134928604+LavakumarPS@users.noreply.github.com>
2023-12-12 16:36:25 +0530

    Prime or Not

---
+++ primeOrNot.js
@@ -0,0 +1,18 @@
+class Solution {
+
+    isPrime(n){
+        
+        //code here
+        if(n<=1){
+            return 0 ;
+        }
+       for(let i=2;i<=Math.sqrt(n);i++)
+       if(n%i==0)
+       return 0;
+       
+     return 1;  
+        
+        
+        
+    }
+}
